5e4c7c5eb9313ae5cb6b8b22956fb8fba6c44bdc
Kibeom Kim <terr76.kim@gmail.com>
2017-10-05 11:20:01 -0400

    Show, Edit, Update, Destroy added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var app = express();
 
 // DB setting
 // MONGO_DB: environment variable from local pc
-mongoose.connect("mongodb://dbuser:dbuser@ds023435.mlab.com:23435/contact_book", {
+mongoose.connect("process.env.MONGO_DB", {
   useMongoClient: true
 });
 var db = mongoose.connection;
@@ -14,6 +15,7 @@ var db = mongoose.connection;
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 var contactSchema = mongoose.Schema({
   name:{type:String, required:true, unique:true},
@@ -34,12 +36,41 @@ app.get("/contacts", function(req, res){
     res.render("contacts/index", {contacts:contacts});
   });
 });
+// Contacts - new
 app.get("/contacts/new", function(req, res){
   res.render("contacts/new");
 });
 // Contacts - create
 app.post("/contacts", function(req, res){
   Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect("/contacts");
+  });
+});
+// Contacts - show
+app.get("/contacts/:id", function(req,res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render("contacts/show", {contact:contact});
+  });
+});
+// Contacts - edit
+app.get("/contacts/:id/edit", function(req, res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render("contacts/edit", {contact:contact});
+  });
+});
+// Contacts - update
+app.put("/contacts/:id", function(req, res){
+  Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect("/contacts/"+req.params.id);
+  });
+});
+// Contacts - delete
+app.delete("/contacts/:id", function(req, res){
+  Contact.remove({_id:req.params.id}, function(err, contact){
     if(err) return res.json(err);
     res.redirect("/contacts");
   });

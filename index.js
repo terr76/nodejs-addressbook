da7a7b825267ad4977ab12309b21e0437c60ae9e
Kibeom Kim <terr76.kim@gmail.com>
2017-10-05 10:26:01 -0400

    index, new, create added

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var app = express();
 
-//DB setting
-//environment variable: MONGO_DB
-mongoose.connect("process.env.MONGO_DB", {
+// DB setting
+// MONGO_DB: environment variable from local pc
+mongoose.connect("mongodb://dbuser:dbuser@ds023435.mlab.com:23435/contact_book", {
   useMongoClient: true
 });
 var db = mongoose.connection;
 
-db.once("open", function(){
-  console.log("DB connected");
-});
-
-db.on("error", function(err){
-  console.log("DB error: ", err);
-});
-
+// Other setting
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String},
+  phone:{type:String}
+});
+var Contact = mongoose.model("contact", contactSchema);
+
+// Routes
+// Home
+app.get("/", function(req, res){
+  res.redirect("/contacts");
+});
+// Contacts - Index
+app.get("/contacts", function(req, res){
+  Contact.find({}, function(err, contacts){
+    if(err) return res.json(err);
+    res.render("contacts/index", {contacts:contacts});
+  });
+});
+app.get("/contacts/new", function(req, res){
+  res.render("contacts/new");
+});
+// Contacts - create
+app.post("/contacts", function(req, res){
+  Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect("/contacts");
+  });
+});
+
+// Port setting
 app.listen(3000, function(){
   console.log("server on");
 });
